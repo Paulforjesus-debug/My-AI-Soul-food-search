49513659a83f2fc184e234941dbc8edaef89d64f
Paulforjesus-debug <icdithnxgod@gmail.com>
2026-09-21 23:45:56 +0900

    fix: use placeholders for initial search examples

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mise-shell-v13';
+const CACHE_NAME = 'mise-shell-v14';
 const APP_SHELL = ['./', './favicon.svg', './mise-featured-dish.png', './manifest.webmanifest', './app-config.js', './neon-client.js?v=7'];
 
 self.addEventListener('install', (event) => {
